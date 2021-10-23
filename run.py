3b785c7ddbbe5858da01421fa7891a3e88d85ce5
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-10-23 15:58:54 +0300

    CREATES FUNCTIONS THAT IMPLEMENT THE BEHAVIOUR CREATED IN THE CREDENTIALS.PY FILE

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.8
 from user import Users
+from credentials import Credentials
 
 def register_user(username, password):
     '''
@@ -8,11 +9,24 @@ def register_user(username, password):
     new_user = Users(username, password)
     return new_user
 
+def add_credential(platform, login_username, login_password):
+    '''
+    Function that creates a new login credential.
+    '''
+    new_credential = Credentials(platform, login_username, login_password)
+    return new_credential
+
 def save_user(user_log):
     '''
     Function that saves the user record.
     '''
     user_log.save_user()
+    
+def save_credential(login_credential):
+    '''
+    Function that saves new credentials.
+    '''
+    login_credential.save_credential()
     
 def delete_user(user_log):
     '''
@@ -20,21 +34,42 @@ def delete_user(user_log):
     '''
     user_log.delete_user()
     
+def delete_credential(login_credential):
+    '''
+    Function that deletes a credential.
+    '''
+    login_credential.delete_credential()
+    
 def check_existing_users(username, password):
     '''
     Function that checks if a contact exists with the username and password and returns a boolean
     '''
     return Users.user_exist(username, password)
 
+def check_existing_credentials(platform):
+    '''
+    Function that checkes if login credentials to a platform exists and returns a boolean.
+    '''
+    return Credentials.credential_exist(platform)
+
+def find_credential(platform):
+    '''
+    Function that finds a credential by platform and returns the login credentials.
+    '''
+    return Credentials.find_credential_by_platform(platform)
+
+def display_all_saved_credentials():
+    '''
+    Function that displays all the saved login credentials.
+    '''
+    return Credentials.display_all_credentials()
 ##################################################################################################
 # The main function begins here
-# from goto import goto, label
 def main():
     print("Hello There! Welcome to the Safest Password Locker")
     print("\n")
     print("\n")
     while True:
-        # label .nowLogin
         print("Use the following short-code commands for easy navigation")
         print("lg -------------- Login")
         print("EX -------------- Exit application")
@@ -61,7 +96,6 @@ def main():
                 save_user(register_user(username, password)) # Creates and saves the user login record
                 print("Account created successfully")
                 
-                # goto .nowLogin #Switches the user back to the login section
             else:
                 print("Login with you credentials")
                 print("\n")
